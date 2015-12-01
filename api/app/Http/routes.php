1f16108f32d4ed62876c16d3e64d2332fa63a69f
Alan Monger <haveacigaro@gmail.com>
2015-12-01 13:24:48 +0000

    Add instructions

--- api/app/Http/routes.php
+++ api/app/Http/routes.php
@@ -14,9 +14,21 @@
 use Illuminate\Http\Request;
 use Knp\Snappy\Pdf;
 
+$app->get('/', function() use ($app) {
+    echo "Make a post request to either /html or /url";
+});
+
+$app->get('/html', function () use ($app) {
+    echo "Use post with a base64 encoded html parameter";
+});
+
 $app->post('/html', function (Request $request) use ($app) {
     $pdf = new Pdf('xvfb-run -a -s "-screen 0 640x480x16" wkhtmltopdf');
     echo base64_encode($pdf->getOutputFromHtml(base64_decode($request->get('html'))));
+});
+
+$app->get('/url', function () use ($app) {
+    echo "Use post with a url parameter";
 });
 
 $app->post('/url', function (Request $request) use ($app) {
